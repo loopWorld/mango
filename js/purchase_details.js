065c59fcdbdf9f7393c7cda4e25643ebb066fd66
xiaoheizi <xiaohei_lailo@163.com>
2024-11-02 23:16:41 +0800

    mangopush

--- js/purchase_details.js
+++ js/purchase_details.js
@@ -15,17 +15,19 @@
     //     <button class="my_btn" style="border: none;width: 50px;height: 50px;border-radius: 50%;background: pink;">开始</button>
     // </div>
     // `
-    let arr = [], index = 0;
+    let a = localStorage.getItem('my_a'), index = localStorage.getItem('my_index');
+    if (a == 1) fn1();
     document.addEventListener('keyup', e => {
-        let { key } = e
-        if (key == 'Delete') {
-            getData()
-        }
+        let { key } = e;
+        if (key == 'Delete') if (a == 0) getData(index)
     })
     async function fn(data) {
-        document.getElementById("keywords").value = data[index];
+        document.getElementById("keywords").value = data;
+        localStorage.setItem('my_a', 1);
         document.querySelector("#mod-frm-search > div > div > div.form-group.submit-container > button").click();
-        await sleep(3000)
+    }
+
+    async function fn1() {
         let imgName = generateRandomInteger(10);
         fetch('http://127.0.0.1:5001/api/printScreen?imgName=' + imgName)
             .then(response => {
@@ -35,15 +37,51 @@
                 return response.json(); // 假设服务器返回JSON格式的数据
             })
             .then(data => {
-                console.log('请求成功:', data);
+                // 单价
+                let price = document.querySelector("#listBox > ul > li > div.order-detail > div:nth-child(2) > table > tbody > tr > td.detail > table > tbody > tr:nth-child(1) > td.s3 > div > span").innerText * 1
+                let count_list = document.querySelectorAll("#listBox > ul > li > div.order-detail > div:nth-child(2) > table > tbody > tr > td.detail > table > tbody > tr > td.s4 > div")
+                // 总数
+                let count = 0;
+                for (let i = 0; i < count_list.length; i++)count += count_list[i].innerText * 1
+                // 总价
+                let price1 = document.querySelector("#listBox > ul > li > div.order-detail > div:nth-child(2) > table > tbody > tr > td.s6 > div.repayment-wrapper > span.repayment") ? parseFloat(document.querySelector("#listBox > ul > li > div.order-detail > div:nth-child(2) > table > tbody > tr > td.s6 > div.repayment-wrapper > span.repayment").innerText.replace(/[^\d.]/g, '')) : document.querySelector("#listBox > ul > li > div.order-detail > div:nth-child(2) > table > tbody > tr > td.s6 > div.total").innerText
+                // 运费
+                let freight = document.querySelector("#listBox > ul > li.item-active.order-item.fd-clr.first > div.order-detail > div:nth-child(2) > table > tbody > tr > td.s6 > div.repayment-wrapper > span.fare") ? parseFloat(document.querySelector("#listBox > ul > li.item-active.order-item.fd-clr.first > div.order-detail > div:nth-child(2) > table > tbody > tr > td.s6 > div.repayment-wrapper > span.fare").innerText.replace(/[^\d.]/g, '')) : 0;
+                // 是否退款
+                let istuihuo = document.querySelector("#listBox > ul > li:nth-child(3) > div.order-detail > div:nth-child(2) > table > tbody > tr > td.detail > table > tbody > tr:nth-child(1) > td.s9 > div:nth-child(1) > a") ? 1 : 0;
+                let data1 = {
+                    "price": price,
+                    "count": count,
+                    "price1": price1,
+                    "freight": freight,
+                    "istuihuo": istuihuo,
+                    "imgName": imgName,
+                    "new_row": index * 1 + 2
+                }
+                let params = new URLSearchParams(data1).toString();
+                fetch(`http://127.0.0.1:5001/api/fill_excel?${params}`)
+                    .then(response => {
+                        if (!response.ok) {
+                            throw new Error(`HTTP error! status: ${response.status}`);
+                        }
+                        return response.json(); // 假设返回的是JSON数据
+                    })
+                    .then(result => {
+                        localStorage.setItem('my_index', index * 1 + 1);
+                        getData(index * 1 + 1)
+                    })
+                    .catch(error => {
+                        localStorage.setItem('my_a', 0);
+                        console.error('结束了');
+                    });
             })
             .catch(error => {
                 console.error('请求出错:', error);
             });
     }
 
-    async function getData() {
-        fetch('http://127.0.0.1:5001/api/get_excel_data')
+    async function getData(index) {
+        fetch('http://127.0.0.1:5001/api/get_excel_data?index=' + index)
             .then(response => {
                 if (!response.ok) {
                     throw new Error(`HTTP error! status: ${response.status}`);
@@ -51,6 +89,7 @@
                 return response.json(); // 假设返回的是JSON数据
             })
             .then(result => {
+                if(result.data == '')return alert("结束了");
                 fn(result.data); // 将 data 属性传入函数 fn
             })
             .catch(error => {
