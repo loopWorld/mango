c16774ac7e2ce059f0121eb9e0f3d62923933954
xiaoheizi <xiaohei_lailo@163.com>
2024-11-03 23:58:57 +0800

    mangopush

--- js/Compliance_Center.js
+++ js/Compliance_Center.js
@@ -11,24 +11,104 @@
 (function () {
     'use strict';
     document.addEventListener('keyup', e => {
-        let { key } = e
+        let { key } = e, arr;
         if (key == 'Delete') {
             if (!localStorage.getItem('Identification_code')) {
                 let code = prompt('请输入识别码')
                 localStorage.setItem('Identification_code', code)
             }
-            let arr = [
+            arrfn()
+            for (let item of arr) fn(item)
+        }
+        if (key == 'Enter') {
+            if (!localStorage.getItem('Identification_code')) {
+                let code = prompt('请输入识别码')
+                localStorage.setItem('Identification_code', code)
+            }
+            fn1();
+        }
+        /**
+         * 模拟按键事件
+         * @param {Document} doc - 文档对象
+         * @param {number} keyCode - 要模拟的按键的键码
+         * @returns {void} - 无返回值
+         */
+        function simulateKeyPress(doc, keyCode) {
+            const event = new KeyboardEvent('keydown', {
+                keyCode: keyCode,
+                code: keyCode,
+                key: keyCode,
+                which: keyCode,
+                bubbles: true,
+                cancelable: true,
+            });
+            doc.dispatchEvent(event);
+        }
+
+        /**
+         * 延迟指定的毫秒数
+         * @param {number} ms - 要延迟的毫秒数
+         * @returns {Promise} - 一个 Promise，它会在指定的毫秒数后 resolve
+         */
+        async function sleep(ms) {
+            return new Promise(resolve => setTimeout(resolve, ms));
+        }
+
+        function fn(item) {
+            let { title, type, select, doc } = item;
+            if (!doc || doc.length == 0) return;
+            if (type === 'search') {
+                doc.focus();
+                // 键盘循环模拟按下向下键                           
+                for (let i = 0; i < select; i++)simulateKeyPress(doc, 40);
+                // 键盘模拟按下回车键
+                simulateKeyPress(doc, 13);
+                return
+            }
+            if (type === 'radio') {
+                doc[select].click();
+                return
+            }
+            if (type == 'text') {
+                doc.focus();
+                document.execCommand('insertText', false, select);
+                return
+            }
+            if( type == 'span') {
+                doc.click();
+                return
+            }
+        }
+        function fn1() {
+            document.querySelectorAll('.rocket-btn.rocket-btn-link.rocket-btn-sm')[0].click()
+            setTimeout(() => {
+                arrfn();
+                for (let item of arr) fn(item);
+                setTimeout(() => {
+                    document.querySelectorAll('.rocket-btn-primary')[1].click()
+                    setTimeout(() => {
+                        document.querySelector("#agentseller-layout-content > div > div > div > form > div > div.rocket-space.rocket-space-horizontal.rocket-space-align-start > div:nth-child(1) > button")
+                        setTimeout(() => {
+                            fn1()
+                        },1500)
+                    },800)
+                },500)
+            }, 4000)
+        }
+
+        function arrfn() {
+            arr = [
                 {
                     title: '警示类型',
                     type: 'search',
                     select: 1,
-                    doc: document.getElementById("4_1000000001") || null
+                    // doc: document.getElementById("4_1000000001") || null
                 },
                 {
                     title: '欧盟负责人',
                     type: 'search',
                     select: 1,
-                    doc: document.getElementById("25_repIdList") || null
+                    // doc: document.getElementById("25_repIdList") || null
                 },
                 {
                     title: '是否含电池',
@@ -40,7 +120,7 @@
                     title: '制造商信息',
                     type: 'search',
                     select: 1,
-                    doc: document.getElementById("60_repIdList") || null
+                    // doc: document.getElementById("60_repIdList") || null
                 },
                 {
                     title: '制造商',
@@ -91,61 +171,18 @@
                     doc: document.getElementById("42_1000100110") || null
                 },
                 {
+                    title: '删除识别码',
+                    type: 'span',
+                    select: 0,
+                    doc: document.querySelector(".rocket-input-suffix > i") || null
+                },
+                {
                     title: '商品识别码',
                     type: 'text',
                     select: localStorage.getItem('Identification_code'),
                     doc: document.getElementById("611100100115List_0_name") || null
                 }
             ];
-            for (let item of arr) fn(item)
-        }
-        /**
-         * 模拟按键事件
-         * @param {Document} doc - 文档对象
-         * @param {number} keyCode - 要模拟的按键的键码
-         * @returns {void} - 无返回值
-         */
-        function simulateKeyPress(doc, keyCode) {
-            const event = new KeyboardEvent('keydown', {
-                keyCode: keyCode,
-                code: keyCode,
-                key: keyCode,
-                which: keyCode,
-                bubbles: true,
-                cancelable: true,
-            });
-            doc.dispatchEvent(event);
-        }
-
-        /**
-         * 延迟指定的毫秒数
-         * @param {number} ms - 要延迟的毫秒数
-         * @returns {Promise} - 一个 Promise，它会在指定的毫秒数后 resolve
-         */
-        function sleep(ms) {
-            return new Promise(resolve => setTimeout(resolve, ms));
-        }
-
-        function fn(item) {
-            let { title, type, select, doc } = item;
-            if (!doc || doc.length == 0) return;
-            if (type === 'search') {
-                doc.focus();
-                // 键盘循环模拟按下向下键                           
-                for (let i = 0; i < select; i++)simulateKeyPress(doc, 40);
-                // 键盘模拟按下回车键
-                simulateKeyPress(doc, 13);
-                return
-            }
-            if (type === 'radio') {
-                doc[select].click();
-                return
-            }
-            if (type == 'text') {
-                doc.focus();
-                document.execCommand('insertText', false, select);
-                return
-            }
         }
     })
 })();
